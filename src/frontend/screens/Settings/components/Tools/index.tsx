93adb87012e65c7e8216b0cec58ab7da2da80a56
Ariel Juodziukynas <arieljuod@gmail.com>
2024-02-10 13:50:56 -0300

    Tell typescript these places do have a valid runner

--- src/frontend/screens/Settings/components/Tools/index.tsx
+++ src/frontend/screens/Settings/components/Tools/index.tsx
@@ -38,7 +38,7 @@ export default function Tools() {
       tool,
       exe,
       appName,
-      runner
+      runner: runner!
     })
 
     if (tool in toolStates) {
@@ -48,7 +48,7 @@ export default function Tools() {
 
   const handleRunExe = async () => {
     let exe = ''
-    const gameinfo = await getGameInfo(appName, runner)
+    const gameinfo = await getGameInfo(appName, runner!)
     if (!gameinfo) return
     const defaultPath =
       gameinfo.runner === 'sideload' ? undefined : gameinfo.install.install_path
